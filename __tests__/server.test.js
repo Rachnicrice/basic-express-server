77ca30c34a8599a3eea1b24c3b249a6d080f4a24
Rachnicrice <46403371+Rachnicrice@users.noreply.github.com>
2021-08-17 18:57:00 -0400

    added logger and validator tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,25 +2,26 @@ const { server } = require('../src/server.js');
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 
+
 describe('Server API Testing:', () => {
 
-  it('sends 404 error on bad route', () => {
-    return mockRequest.get('/this-is-not-a-real-route')
+  it('sends 404 error on bad route', async () => {
+    return await mockRequest.get('/this-is-not-a-real-route')
       .then(result => {
         expect(result.status).toEqual(404);
       });
   });
 
-  it('sends 500 if no name is in the query string', () => {
-    mockRequest.get('/person')
+  it('sends 500 if no name is in the query string', async () => {
+    return await mockRequest.get('/person')
       .then(result => {
         expect(result.status).toEqual(500);
       });
 
   });
 
-  it('sends 200 on good request w/ name in query string', () => {
-    mockRequest.get('/person?nameProvided=eragon')
+  it('sends 200 on good request w/ name in query string', async () => {
+    return await mockRequest.get('/person?nameProvided=eragon')
       .then(result => {
         expect(result.status).toEqual(200);
       });
